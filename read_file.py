30f5f3721402078f0f18620cc0bdd032b31acdb4
AlexeyLuch <pochtamoyapochta123@gmail.com>
2019-07-04 17:35:49 +0300

    lessons

--- read_file.py
+++ read_file.py
@@ -39,52 +39,96 @@
 #
 # intro(Firstname="Sita", Lastname="Sharma", Age=22, Phone=1234567890)
 # intro(Firstname="John", Lastname="Wood", Email="johnwood@nomail.com", Country="Wakanda", Age=25, Phone=9876543210)
-#
-#
-def rest(**rest):
-    for key,value in rest.items():
-        print("{} are {}".format(key, value))
+# #
+""""""""""""""""""""""""""""""""""""
 
-rest(Alexey="Luch",Alexey2= "Vorchun",Andrey="Kassad")
+
+"""keyword"""
+
+
+# def rest(**rest):
+#     for key,value in rest.items():
+#         print("{} are {}".format(key, value))
+#
+# rest(Alexey="Luch",Alexey2= "Vorchun",Andrey="Kassad")
+
+
+""" swap """
 
 # a = 20
 # b = 21
-#
 # a, b = b, a
-#
 # print("a = %d and b = %d" % (a,b))
+
+""" list to string """
 
 # a = ["sada","reter","kust","veter"]
 # print("it\'s the string==> "+" ".join(a))
-# print(list(map(abs, (-1, 0, 1))))
 
-old_list = [1,2,3,4]
-new_list = list(map(str, old_list))
-print(new_list)
+# print(list(map(abs, (-1, 0, 1))))  """ HZ 4TO """
 
+""" use MAP for make list to string and back """
+
+# old_list = [1,2,3,4]
+# new_list = list(map(str, old_list))
+# print(new_list)
+# old_list = list(map(int, new_list))
+# print(old_list)
+
+""" tuple = кортеж / преобразование """
 # a = tuple('hello, world!')
 # print(a)
+# b = tuple(a)
+# print(b)
+
+""" lambda """
 
 # func = lambda x,y : +x
 # print(func(2,3))
-
-# x = 0
+""" Except """
 # try:
 #     k= 1/0
 #     print("True")
 # except ZeroDivisionError:
 #     print("False")
 
+""" iterating over file’s lines """
+
 # with open("intents.json") as files:
 #     for i in files:
 #         print(i)
 
+"""lists in dict """
+
 # uniq = [1,2,3,4]
 # fifa = ['a','b','c','d','e']
-# print(dict(zip(uniq, fifa)))
+# c = dict(zip(uniq,fifa))
+# print(c)
+
+""" list to string (for) """
+
+# c = str()
+# fifa = ['a','b','c','d','e']
+# for x in fifa:
+#     c+=x
+# print(c)
+# print(type(c))
+
+""" revers """
+
+# fifa = ['2','3','1','2','4']
+# c = fifa[::-1]
+# print(c)
+
+""" узнать путь к импорту """
+
+# import sys
+#
+# print(sys.path)
+
+# a = ["a","s"]
+# b = ["1a","1s","1d"]
+# c = dict(zip(a,b))
+# print(c)
 
 
-
-fifa = ['a','b','c','d','e']
-for x in fifa[:]:
-    print(x)
